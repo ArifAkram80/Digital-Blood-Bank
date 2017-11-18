a13fe944b5fa255897f1e1b3ea30eb16ff06632b
Arif Akram <ArifAkram80@users.noreply.github.com>
2017-11-19 03:02:36 +0600

    Google Loction Class Added & SignUp Location Button Added

--- app/src/main/java/com/example/arif/digitalbloodbank/Signup_form.java
+++ app/src/main/java/com/example/arif/digitalbloodbank/Signup_form.java
@@ -1,11 +1,18 @@
 package com.example.arif.digitalbloodbank;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -14,22 +21,63 @@ public class Signup_form extends AppCompatActivity {
     private Spinner BloodGroup, Gender;
     String bloodGroup = "Choose Blood", gender = "I am";
 
+    Button btnLoc;
+
+    GoogleLocation location ;
+
+    String Loca = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_form);
         setTitle("Registration Form");
-
+        location = new GoogleLocation(this);
         init();
         itemlistener();
 
+        btnLoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                location.GetSingleUpdateLocation();
+                if (location.getLocation_Co_oridinate() != null)
+                {
+                    Loca = location.getLocation_Co_oridinate().toString();
+                }
+            }
+        });
+
+
+
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 111) {
+            Log.i("SignUp", "Permission Under Constraction");
+            if (grantResults != null && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    location.locationManager.requestSingleUpdate(location.criteria, location.locationListener, location.looper);
+                }
+            }
+            else{
+              //  Toast.makeText(this, "Location Is Required", Toast.LENGTH_SHORT).show();
+                Log.i("SignUp", "Permission Not Granted");
+            }
+        }
     }
 
 
     private void init() {
-        BloodGroup = (Spinner) findViewById(R.id.BloodGroup);
 
+        btnLoc = (Button) findViewById(R.id.location);
+
+        BloodGroup = (Spinner) findViewById(R.id.BloodGroup);
 // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.Blood_array, R.layout.custom_spinner);
